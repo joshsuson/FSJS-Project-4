dd6c6d3f4f2623326bb2c336fd0e6f0763a8a8c9
Josh Suson <josh.suson@gmail.com>
2020-01-02 19:55:50 -0500

    Complete phrase class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor() {
+         this.missed = 0; 
+         this.phrases = [
+            "Not all who wander are lost",
+            "It is about the journey not the destination",
+            "For the joy of the Lord is my refuge",
+            "Jesus plus nothing equals everything",
+            "Therego I but for the grace of God"
+         ];
+         this.activePhrase = null;
+     }
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,45 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+     constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+     }
+
+     addPhraseToDisplay() {
+        const phraseUL = document.querySelector('#phrase ul');
+        const phraseLetters = this.phrase.split('');
+
+        phraseLetters.forEach(letter => {
+            let li = document.createElement('li');
+            li.textContent = letter;
+            if (li.textContent !== ' ') {
+                li.classList.add('hide', 'letter', letter);
+            } else {
+                li.classList.add('hide', 'space');
+            }
+            phraseUL.appendChild(li);
+         })
+
+     }
+
+     checkLetter(guess) {
+        const activeLetters = document.getElementsByClassName('letter');
+        let match = null;
+
+        [...activeLetters].forEach(letter => {
+            if (letter.textContent.toLowerCase() === guess.textContent.toLowerCase()) {
+                match = guess; 
+            }
+        });
+        return match;
+     }
+
+     showMatchedLetter(letter) {
+    const matchedLetters = document.querySelectorAll(`.${letter.textContent}`);
+    [...matchedLetters].forEach(match => {
+        match.classList.add('show');
+    });
+ }
+}
